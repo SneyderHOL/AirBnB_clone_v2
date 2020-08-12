54da8fa3cc2f1c7f191dc67a2c422ac54f0fcf18
SneyderHOL <1507@holbertonschool.com>
2020-08-12 16:22:51 +0000

    [UPDATE]: remove repeating lines

--- models/city.py
+++ models/city.py
@@ -8,8 +8,6 @@ from sqlalchemy.orm import relationship, backref
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-
-    name = Column(String(128), nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     state = relationship("State", backref="cities")

1e96abac49a8d9de35c5d7f019ff8fc0d27d5a26
SneyderHOL <1507@holbertonschool.com>
2020-08-12 17:01:43 +0000

    [UPDATE]: comment line

--- models/place.py
+++ models/place.py
@@ -18,7 +18,7 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
+#    amenity_ids = []
     cities = relationship("City",
                           backref=backref("places",
                                           cascade="all, delete-orphan"))

6ba242922ed48164852a62bf9cbd93eb9c500c9a
SneyderHOL <1507@holbertonschool.com>
2020-08-13 04:17:38 +0000

    [UPDATE]: add missing variables to kwargs in init method

--- models/base_model.py
+++ models/base_model.py
@@ -18,12 +18,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        dates = ['created_at', 'updated_at']
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
+            if 'id' not in kwargs.keys():
+                kwargs['id'] = str(uuid.uuid4())
+                kwargs['created_at'] = datetime.now().isoformat()
+                kwargs['updated_at'] = datetime.now().isoformat()
+                kwargs['__class__'] = self.__class__.__name__
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],

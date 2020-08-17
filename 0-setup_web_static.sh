8dca6d8cbd793de165cc5f1ddc4d6ccd56d52d49
SneyderHOL <1507@holbertonschool.com>
2020-08-17 22:45:40 +0000

    [UPDATE]: add rm command before creation of sym link

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 #Bash script that sets up the web server for the deployment of web_static
-sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get -y update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 echo "Testing configuration" > /data/web_static/releases/test/index.html
 sudo mkdir -p /data/web_static/shared/
+sudo rm -fr /data/web_static/current
 sudo ln -sfT /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i "/default_server;/ a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}/" /etc/nginx/sites-available/default

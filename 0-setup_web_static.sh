6bd1f281d7b738ee35f130873b72ade371c7469c
SneyderHOL <1507@holbertonschool.com>
2020-08-17 23:04:45 +0000

    [UPDATE]: use tee command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,10 @@
 #Bash script that sets up the web server for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "Testing configuration" > /data/web_static/releases/test/index.html
-#sudo rm -fr /data/web_static/current
+sudo mkdir -p /data/web_static/shared/
+echo "Testing configuration" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sfT /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/default_server;/ a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}/" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80 default_server;/ a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart

258b75bdabc6d87da417ffedd157a6ee2cbed147
SneyderHOL <1507@holbertonschool.com>
2020-09-02 00:19:12 +0000

    Add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for start Flask web application"""
+import os
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """hello_hbnb function that show the message Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """hbnb function that show the message HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_func(text):
+    """c_func function that show the message c <text>"""
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

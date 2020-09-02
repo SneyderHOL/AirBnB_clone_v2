c12b7da7eaa93869825a612bdc2b943a41c88e46
SneyderHOL <1507@holbertonschool.com>
2020-09-02 01:58:49 +0000

    Add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module for start Flask web application"""
+import os
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """hello_hbnb function that show the message Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """hbnb function that show the message HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_func(text):
+    """c_func function that show the message c <text>"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_func(text="is cool"):
+    """python_func function that show the message python <text>"""
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

3a10544e18b08ef2a3395993c19c7b1244c4d207
SneyderHOL <1507@holbertonschool.com>
2020-09-02 00:11:33 +0000

    Add task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for start Flask web application"""
+import os
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """hello_hbnb function that show the message Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """hbnb function that show the message HBNB"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

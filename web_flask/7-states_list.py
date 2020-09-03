73646b0937eb3dbb0f86dee0f5ebab39846d2c6a
SneyderHOL <1507@holbertonschool.com>
2020-09-03 02:30:10 +0000

    [UPDATE]: order list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Module for start Flask web application"""
-import os
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -11,17 +13,12 @@ def get_states_list():
     """get the list of states objects in storage"""
     dictionary = storage.all(State)
     states_list = list(dictionary.values())
-    if states_list is None or states_list == []:
-        print("Error with states_list")
-        return None
     return render_template('7-states_list.html', states_list=states_list)
 
 
 @app.teardown_appcontext
-def remove_session():
+def remove_session(exception):
     """remove session after each request"""
-    from models.__init__ import storage
-    from models.state import State
     storage.close()
 
 
